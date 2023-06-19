342b83442b9372b542b012f76b4418bd4e1b3ee7
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-06-19 15:33:45 -0500

    Remove column div class from grid creation
    Was causing the square items to not flex with row
    
    Removing div class caused the items to flex and was an
    extra div that wasn't being used.

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
   function createGrid(){ //Create default grid 16x16
     const container = document.querySelector('.container');
-    const column = document.querySelectorAll('.column');
   
     for(let x = 0; x < 16; x++){ //create rows and columns
       const row = document.createElement('div');
-      const column = document.createElement('div');
-      column.classList.add('.column');
-      row.classList.add('.row');
+      row.classList.add('row');
 
       for(let i = 0; i < 16; i++){ //add squares to column
         const squares = document.createElement('div');
         squares.classList.add('squares');
-        column.appendChild(squares);
+        row.appendChild(squares);
       }
 
-      row.appendChild(column);
       container.appendChild(row);
     }
 
@@ -27,21 +23,19 @@
       while(container.hasChildNodes()){ //Remove all existing grids
         container.removeChild(container.firstChild);
       }
-      for(let x = 0; x < size; x++){ //create rows and columns of size prompted
-        const row = document.createElement('div');
-        const column = document.createElement('div');
-        column.classList.add('.column');
-        row.classList.add('.row');
-
-        for(let i = 0; i < size; i++){ //add squares to column
-          const squares = document.createElement('div');
-          squares.classList.add('squares');
-          column.appendChild(squares);
-        }
   
-        row.appendChild(column);
-        container.appendChild(row);
+    for(let x = 0; x < size; x++){ //create rows and columns
+      const row = document.createElement('div');
+      row.classList.add('row');
+
+      for(let i = 0; i < size; i++){ //add squares to column
+        const squares = document.createElement('div');
+        squares.classList.add('squares');
+        row.appendChild(squares);
       }
+
+      container.appendChild(row);
+    }
     }
     else{
       alert('Invalid input');
